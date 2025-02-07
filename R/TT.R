b9e233ad8b45bb3483ac4163f96e8c33a4259a41
Cumaribo <jeronimo.rescobar@gmail.com>
2025-02-06 17:49:20 -0800

    work on the redrawing of the polygon. Need to be able to really organzie this, can' be that any change is like i am doing this for the forst time in my life

--- R/TT.R
+++ R/TT.R
@@ -8,17 +8,12 @@ gdalbuildvrt(gdalfile = tiffes, output.vrt = paste0(outpath, '/', "global_dem.vr
 
 
 
-output_vrt)
-
-
 tiles <- lapply(tiffes,rast)
 assembled <- do.call(terra::merge, tiles)
 writeRaster(assembled, paste0(outpath,'/', 'DEM_global.tif'))
 
 
-
 lc_rast <- rast('/home/jeronimo/global_ES_modeling/esos-c/data_2/ndr/ESA_LC/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif', lyrs=1)
 writeRaster(lc_rast, '/home/jeronimo/global_ES_modeling/esos-c/data_2/ndr/landcover_gl_1.tif')
-
 
 biph <- read.csv('/home/jeronimo/global_ES_modeling/esos-c/data_2/ndr/esa_biophysical_0916_md5_b2886c.csv')
