ec06ea9b1ee6b40b216bfb4b5155044d25a7c827
Cumaribo <jeronimo.rescobar@gmail.com>
2025-02-08 17:10:26 -0800

    finally fixed and simplified this thing. Pending to think about waht share should i use, (potential intervention, whole thing) Redo the maps without the north shore. Fiox espirito santo later. I think we can merge to main already.

--- R/TT.R
+++ R/TT.R
@@ -1,5 +1,5 @@
 inpath <- '/home/jeronimo/global_ES_modeling/esos-c/data_2/ndr/global_dem_3s_md5_22d0c3809af491fa09d03002bdf09748'
-outpath <- '/home/jeronimo/global_ES_modeling/esos-c/data_2/ndr'
+outpath <- '/home/jeronimo/global_ES_modeling/esos-c/data/ndr'
 output_vrt <- "/path/to/output/global_dem.vrt"
 tiffes <- file.path(list.files(inpath, pattern="\\.tif$", full.names = TRUE))
 
@@ -12,8 +12,17 @@ tiles <- lapply(tiffes,rast)
 assembled <- do.call(terra::merge, tiles)
 writeRaster(assembled, paste0(outpath,'/', 'DEM_global.tif'))
 
+dem <- rast(paste0(outpath,'/', 'dem_global_3s.tif'))
+
+
 
 lc_rast <- rast('/home/jeronimo/global_ES_modeling/esos-c/data_2/ndr/ESA_LC/ESACCI-LC-L4-LCCS-Map-300m-P1Y-1992_2015-v2.0.7.tif', lyrs=1)
 writeRaster(lc_rast, '/home/jeronimo/global_ES_modeling/esos-c/data_2/ndr/landcover_gl_1.tif')
+
+lc_rast <- rast('/home/jeronimo/global_ES_modeling/esos-c/data/ndr/landcover_gl_1.tif')
+
+lc_rast
+
+
 
 biph <- read.csv('/home/jeronimo/global_ES_modeling/esos-c/data_2/ndr/esa_biophysical_0916_md5_b2886c.csv')
